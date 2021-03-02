6c26c7742c655601f3f7761456343e94e8ab3d69
cotlowzky <cotlowzky@gmail.com>
2021-03-02 12:46:13 +0100

    slider v essa

--- Frontend/src/slider.js
+++ Frontend/src/slider.js
@@ -11,30 +11,30 @@ function changeImage(){
     if(i==1){
     heroImage.style.backgroundImage  = "url('../assets/img/undraw_awesome_rlvy-min.png')";
     description.innerHTML = "Jako zalogowany użytkownik <br /> masz dostęp do większej palety funkcjonalności!";
-    firstBar.classList.remove('bg-info');
-    firstBar.classList.add('bg-primary');
-    secondBar.classList.add('bg-info');
-    thirdBar.classList.add('bg-info');
+    firstBar.classList.remove('bg-info', 'noMainBtn');
+    firstBar.classList.add('bg-primary','mainBtn');
+    secondBar.classList.add('bg-info', 'noMainBtn');
+    thirdBar.classList.add('bg-info', 'noMainBtn');
     i++;
     }
     //second bar
     else if(i==2){
     heroImage.style.backgroundImage  = "url('../assets/img/undraw_pending_approval_xuu9-min.png')";
     description.innerHTML = "Jako zalogowany użytkownik <br /> otrzymujesz dostęp do wysłania swojej aplikacji";
-    firstBar.classList.add('bg-info');
-    secondBar.classList.remove('bg-info');
-    secondBar.classList.add('bg-primary');
-    thirdBar.classList.add('bg-info');
+    firstBar.classList.add('bg-info', 'noMainBtn');
+    secondBar.classList.remove('bg-info', 'noMainBtn');
+    secondBar.classList.add('bg-primary','mainBtn');
+    thirdBar.classList.add('bg-info', 'noMainBtn');
     i++;
     }
     //third bar
     else if(i==3){  
     heroImage.style.backgroundImage  = "url('../assets/img/undraw_Resume_folder_re_e0bi-min.png')";
     description.innerHTML = "Jako zalogowany użytkownik <br /> możesz przeglądać otrzymane oferty zgłoszeń";
-    firstBar.classList.add('bg-info');
-    secondBar.classList.add('bg-info');
-    thirdBar.classList.remove('bg-info');
-    thirdBar.classList.add('bg-primary');
+    firstBar.classList.add('bg-info', 'noMainBtn');
+    secondBar.classList.add('bg-info', 'noMainBtn');
+    thirdBar.classList.remove('bg-info', 'noMainBtn');
+    thirdBar.classList.add('bg-primary','mainBtn');
     i =1;
     }
 }
@@ -43,27 +43,27 @@ setInterval(changeImage,5000);
 firstBar.addEventListener('click', ()=>{
     heroImage.style.backgroundImage  = "url('../assets/img/undraw_awesome_rlvy-min.png')";
     description.innerHTML = "Jako zalogowany użytkownik <br /> masz dostęp do większej palety funkcjonalności!";
-    firstBar.classList.remove('bg-info');
-    firstBar.classList.add('bg-primary');
-    secondBar.classList.add('bg-info');
-    thirdBar.classList.add('bg-info');
+    firstBar.classList.remove('bg-info', 'noMainBtn');
+    firstBar.classList.add('bg-primary','mainBtn');
+    secondBar.classList.add('bg-info', 'noMainBtn');
+    thirdBar.classList.add('bg-info', 'noMainBtn');
     i = 1;
 });
 secondBar.addEventListener('click', ()=>{
     heroImage.style.backgroundImage  = "url('../assets/img/undraw_pending_approval_xuu9-min.png')";
     description.innerHTML = "Jako zalogowany użytkownik <br /> otrzymujesz dostęp do wysłania swojej aplikacji";
-    firstBar.classList.add('bg-info');
-    secondBar.classList.remove('bg-info');
-    secondBar.classList.add('bg-primary');
-    thirdBar.classList.add('bg-info');
+    firstBar.classList.add('bg-info', 'noMainBtn');
+    secondBar.classList.remove('bg-info', 'noMainBtn');
+    secondBar.classList.add('bg-primary','mainBtn');
+    thirdBar.classList.add('bg-info', 'noMainBtn');
     i = 2;
 });
 thirdBar.addEventListener('click', ()=>{
     heroImage.style.backgroundImage  = "url('../assets/img/undraw_Resume_folder_re_e0bi-min.png')";
     description.innerHTML = "Jako zalogowany użytkownik <br /> możesz przeglądać otrzymane oferty zgłoszeń";
-    firstBar.classList.add('bg-info');
-    secondBar.classList.add('bg-info');
-    thirdBar.classList.remove('bg-info');
-    thirdBar.classList.add('bg-primary');
+    firstBar.classList.add('bg-info', 'noMainBtn');
+    secondBar.classList.add('bg-info', 'noMainBtn');
+    thirdBar.classList.remove('bg-info', 'noMainBtn');
+    thirdBar.classList.add('bg-primary','mainBtn');
     i = 3;
 });
