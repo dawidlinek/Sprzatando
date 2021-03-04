eab881149bc0774d40284b52c3a4c89f7034b89a
dawidlinek123 <56173086+dawidlinek123@users.noreply.github.com>
2021-03-04 11:47:44 +0100

    weryfikacja email

--- Backend/Sprzatando/routes/web.php
+++ Backend/Sprzatando/routes/web.php
@@ -29,7 +29,7 @@ Route::get('/email/verify', function () {
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
     return redirect('/dashboard');
-})->name('verification.verify');
+})->middleware(['auth', 'signed'])->name('verification.verify');
  
 Route::post('/email/verification-notification', function (Request $request) {
     $request->user()->sendEmailVerificationNotification();
