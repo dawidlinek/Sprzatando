d1bac0d8a0a72dad55785e07c83e531f0a64308c
dawidlinek123 <56173086+dawidlinek123@users.noreply.github.com>
2021-03-04 13:14:30 +0100

    navbar

--- Backend/Sprzatando/resources/views/auth/navbar.blade.php
+++ Backend/Sprzatando/resources/views/auth/navbar.blade.php
@@ -1,20 +1,21 @@
+
 <nav class="navbar navbar-light pb-3">
     <a class="navbar-brand" href="#">
         <img src="" width="50" height="50" alt="LOGO" />
     </a>
     <div class="d-flex justify-content-end">
         <ul class="navbar-nav flex-row">
-            <li class="nav-item active">
+            <li class="nav-item">
                 <a class="nav-link" href="{{ route('login') }}">
                     <span
-                    class="sr-only d-none d-lg-block   @if($route=='login')  text-primary @endif" 
+                        class="sr-only @if($route=='login')  text-primary d-none d-lg-block @endif"
                         style="margin-right: 3rem"
                         >Zaloguj się!</span
                     >
                 </a>
             </li>
             <li class="nav-item">
-             <a class="nav-link" href="{{ route('register') }}"> <span @if($route=='register')  class="text-primary" @endif> Rejestracja </span></a>  
+                <a class="nav-link @if($route=='register') d-none d-lg-block text-primary @endif"  href="{{ route('register') }}">Rejestracja</a>
             </li>
         </ul>
     </div>
