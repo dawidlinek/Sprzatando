020c4bc2c51e9e7a7502cb1c8f948e1c8e794f16
dawidlinek123 <56173086+dawidlinek123@users.noreply.github.com>
2021-03-06 18:21:20 +0100

    title

--- Backend/Sprzatando/resources/views/dashboard/main_layout.blade.php
+++ Backend/Sprzatando/resources/views/dashboard/main_layout.blade.php
@@ -86,26 +86,7 @@
                     </div>
 
                     <!-- Sidebar links -->
-                    <ul class="nav flex-column">
-                        <li class="nav-item m-1">
-                            <a class="nav-link rounded" href="{{route('announcement.create')}}">
-                                <span data-feather="home"></span>
-                                Dodaj ogłoszenie
-                            </a>
-                        </li>
-                        <li class="nav-item m-1">
-                            <a class="nav-link rounded" href="{{route('announcement.index')}}">
-                                <span data-feather="home"></span>
-                                Moje ogłoszenia
-                            </a>
-                        </li>
-                        <li class="nav-item m-1">
-                            <a class="nav-link bg-primary text-white rounded" href="#">
-                                <span data-feather="file"></span>
-                                Ustawienia
-                            </a>
-                        </li>
-                    </ul>
+          @include('dashboard.menu')
                 </div>
             </nav>
 
---
+++ Backend/Sprzatando/resources/views/dashboard/menu.blade.php
@@ -0,0 +1,20 @@
+<ul class="nav flex-column">
+    <li class="nav-item m-1">
+        <a class="nav-link rounded" href="{{route('announcement.create')}}">
+            <span data-feather="home"></span>
+            Dodaj ogłoszenie
+        </a>
+    </li>
+    <li class="nav-item m-1">
+        <a class="nav-link rounded" href="{{route('announcement.index')}}">
+            <span data-feather="home"></span>
+            Moje ogłoszenia
+        </a>
+    </li>
+    <li class="nav-item m-1">
+        <a class="nav-link bg-primary text-white rounded" href="/user/profile">
+            <span data-feather="file"></span>
+            Ustawienia
+        </a>
+    </li>
+</ul>
