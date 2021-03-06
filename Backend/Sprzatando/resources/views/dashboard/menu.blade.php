818ae1fa4d833daacbce159fefbbb96d71250cea
dawidlinek123 <56173086+dawidlinek123@users.noreply.github.com>
2021-03-06 18:30:12 +0100

    sidebar

--- Backend/Sprzatando/resources/views/dashboard/menu.blade.php
+++ Backend/Sprzatando/resources/views/dashboard/menu.blade.php
@@ -1,18 +1,18 @@
 <ul class="nav flex-column">
     <li class="nav-item m-1">
-        <a class="nav-link rounded" href="{{route('announcement.create')}}">
+
+        <a class="nav-link rounded @if(request()->route()->uri=='announcement/create') bg-primary text-white @endif" href="{{route('announcement.create')}}">
             <span data-feather="home"></span>
-            Dodaj ogłoszenie
-        </a>
+            Dodaj ogłoszenie         </a>
     </li>
     <li class="nav-item m-1">
-        <a class="nav-link rounded" href="{{route('announcement.index')}}">
+        <a class="nav-link rounded @if(request()->route()->uri=='announcement') bg-primary text-white @endif" href="{{route('announcement.index')}}">
             <span data-feather="home"></span>
             Moje ogłoszenia
         </a>
     </li>
     <li class="nav-item m-1">
-        <a class="nav-link bg-primary text-white rounded" href="/user/profile">
+        <a class="nav-link rounded @if(request()->route()->uri=='user/profile') bg-primary text-white @endif" href="/user/profile">
             <span data-feather="file"></span>
             Ustawienia
         </a>
