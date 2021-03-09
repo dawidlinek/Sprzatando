0017ee55a8ce89d5276e5752ed9906ae128bfebe
D3vmotiv <czechpiotr2701@gmail.com>
2021-03-09 09:19:21 +0100

    zmiana kolorow2

--- Backend/Sprzatando/resources/views/auth/login.blade.php
+++ Backend/Sprzatando/resources/views/auth/login.blade.php
@@ -30,43 +30,43 @@
                     <div class="alert alert-success mb-2 mt-4 col-10 mx-auto border-0 p-2" role="alert">
                         {{ session('status') }}
                     </div>
-                @endif
-                <form method="POST" action="{{ route('login') }}">
-                    @csrf
-                    <div class="form-group pb-0">
+                    @endif
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <div class="form-group pb-0">
 
-                       
-                        @include('auth.errors',["errors"=>$errors])
-                        <div class="b-col col-10 mx-auto">
-                            <label for="FormControlInput1 col-offset">Email</label>
-                            <input name="email" value="{{ old('email') }}" autofocus required type="email" class="form-control mb-4" id="FormControlInput1" placeholder="twój@adres.com" />
-                        </div>
-                        <div class="b-col col-10 mx-auto">
-                            <div class="d-flex flex-row justify-content-between">
-                                <label for="FormControlInput2">Hasło</label>
-                                <a href="{{ route('password.request') }}" style="text-decoration: none">Przywróć hasło</a>
+
+                            @include('auth.errors',["errors"=>$errors])
+                            <div class="b-col col-10 mx-auto">
+                                <label for="FormControlInput1 col-offset">Email</label>
+                                <input name="email" value="{{ old('email') }}" autofocus required type="email" class="form-control mb-4" id="FormControlInput1" placeholder="twój@adres.com" />
                             </div>
-                            <input name="password" required autocomplete="current-password" type="password" class="form-control mb-4" id="FormControlInput2" />
+                            <div class="b-col col-10 mx-auto">
+                                <div class="d-flex flex-row justify-content-between">
+                                    <label for="FormControlInput2">Hasło</label>
+                                    <a href="{{ route('password.request') }}" style="text-decoration: none">Przywróć hasło</a>
+                                </div>
+                                <input name="password" required autocomplete="current-password" type="password" class="form-control mb-4" id="FormControlInput2" />
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="form-check pb-2 mt-0 mb-5 col-10 mx-auto">
-                        <div class="b-col col-6">
-                            <input name="remember" type="checkbox" class="form-check-input" id="Check1" />
-                            <label class="form-check-label" for="Check1">Pamiętaj mnie</label>
+                        <div class="form-check pb-2 mt-0 mb-5 col-10 mx-auto">
+                            <div class="b-col col-6">
+                                <input name="remember" type="checkbox" class="form-check-input" id="Check1" />
+                                <label class="form-check-label" for="Check1">Pamiętaj mnie</label>
+                            </div>
                         </div>
-                    </div>
-                    <div class="text-center col-10 mx-auto">
-                        <button type="submit" class="btn input-block-level form-control center-block btn-primary">
-                            Zaloguj!
-                        </button>
-                    </div>
-                </form>
+                        <div class="text-center col-10 mx-auto">
+                            <button type="submit" class="btn input-block-level form-control center-block btn-primary text-white">
+                                Zaloguj!
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
+            <!-- </Form> -->
         </div>
-        <!-- </Form> -->
-    </div>
-    <!-- </Right column> -->
+        <!-- </Right column> -->
     </div>
 
 </body>
--- Backend/Sprzatando/resources/views/dashboard/my_announcements.blade.php
+++ Backend/Sprzatando/resources/views/dashboard/my_announcements.blade.php
@@ -1,83 +1,81 @@
+@extends('dashboard.main_layout')
 
-  @extends('dashboard.main_layout')
+@section('main')
 
-  @section('main')
+<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 bg-light">
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
+        <!-- MAIN CONTENT START -->
+        <div class="card w-100">
+            <div class="card-body d-flex flex-column align-items-start justify-content-between">
+                <div class="cart-title col-12">
+                    <h2 class="text-primary mb-4">Moje ogłoszenia</h2>
+                </div>
+                <!-- POJEDYNCZE OGŁOSZENIE -->
+                @foreach ($announcements as $announcement)
 
-            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 bg-light">
-                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
-                    <!-- MAIN CONTENT START -->
-                    <div class="card w-100">
-                        <div class="card-body d-flex flex-column align-items-start justify-content-between">
-                            <div class="cart-title col-12">
-                                <h2 class="text-primary mb-4">Moje ogłoszenia</h2>
-                            </div>
-                            <!-- POJEDYNCZE OGŁOSZENIE -->
-                            @foreach ($announcements as $announcement)
-                                
-                         
-                            <div class="card d-flex w-100 mt-3">
-                                <div class="row w-100 mx-auto">
-                                    <div class="col-md-2 rounded"
-                                        style="background-image: url(https://assets.archon.pl/images/products/mfe42422780eaf/widok-1-projekt-dom-w-balsamowcach-2-8719685424c5e520fa9a1911a47e76a0__289.jpg); background-position: center center; background-size: cover; min-height: 200px;">
-                                        &nbsp;
+
+                <div class="card d-flex w-100 mt-3">
+                    <div class="row w-100 mx-auto">
+                        <div class="col-md-2 rounded" style="background-image: url(https://assets.archon.pl/images/products/mfe42422780eaf/widok-1-projekt-dom-w-balsamowcach-2-8719685424c5e520fa9a1911a47e76a0__289.jpg); background-position: center center; background-size: cover; min-height: 200px;">
+                            &nbsp;
+                        </div>
+                        <div class="col-md-8 col-sm-8">
+                            <div class="card-body">
+                                <div class="row col-12">
+                                    <div class="col-lg-6">
+                                        <h5 class="card-title text-primary text-nowrap">{{$announcement->title}}</h5>
                                     </div>
-                                    <div class="col-md-8 col-sm-8">
-                                        <div class="card-body">
-                                            <div class="row col-12">
-                                                <div class="col-lg-6">
-                                                    <h5 class="card-title text-primary text-nowrap">{{$announcement->title}}</h5>
-                                                </div>
-                                                <div class="col-lg-4">
-                                                    <p class="card-text nowrap d-sm-flex"><small class="text-muted d-md-none justify-content-start">{{$announcement->localization}}</small><small class="text-muted d-flex justify-content-around">{{$announcement->price}} zł</small>
-                                                    </p>
-                                                    
-                                                </div>
-                                            </div>
-                                            <div class="w-100">
-                                                <p class="card-text">
-                                                    <b-badge><small class="text-danger">b-badge {{$announcement->status}}</small></b-badge>
-                                                </p>
-                                            </div>
-
-                                            <p class="card-text">{{$announcement->description}} </p>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-2 p-4">
-                                        <div class="b-row d-md-block d-none">
-                                            <p><small class="text-muted overflow-wrap">{{$announcement->localization}}</small></p>
-                                        </div>
-                                        <div class="b-row d-flex align-text-center p-3">
-                                            <button class="btn btn-primary w-100 text-nowrap" href='/announcement/{{$announcement->id}}'>Edytuj</button>
-                                        </div>
+                                    <div class="col-lg-4">
+                                        <p class="card-text nowrap d-sm-flex"><small class="text-muted d-md-none justify-content-start">{{$announcement->localization}}</small><small class="text-muted d-flex justify-content-around">{{$announcement->price}} zł</small>
+                                        </p>
 
                                     </div>
                                 </div>
-                            </div>
-                            <!-- POJEDYNCZE OGŁOSZENIE END -->
-                            @endforeach
-                        </div>
+                                <div class="w-100">
+                                    <p class="card-text">
+                                        <b-badge><small class="text-danger">b-badge {{$announcement->status}}</small></b-badge>
+                                    </p>
+                                </div>
 
+                                <p class="card-text">{{$announcement->description}} </p>
+                            </div>
+                        </div>
+                        <div class="col-md-2 p-4">
+                            <div class="b-row d-md-block d-none">
+                                <p><small class="text-muted overflow-wrap">{{$announcement->localization}}</small></p>
+                            </div>
+                            <div class="b-row d-flex align-text-center p-3">
+                                <button class="btn btn-primary w-100 text-nowrap text-white" href='/announcement/{{$announcement->id}}'>Edytuj</button>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
-                <div class="fixed-bottom offset-md-2 p-4">
-                    <nav aria-label="Strony">
-                        <ul class="pagination justify-content-center">
-                            <li class="page-item disabled">
-                                <a class="page-link" href="#" tabindex="-1">Poprzednia</a>
-                            </li>
-                            <li class="page-item"><a class="page-link" href="#">1</a></li>
-                            <li class="page-item"><a class="page-link" href="#">2</a></li>
-                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                            <li class="page-item">
-                                <a class="page-link" href="#">Następna</a>
-                            </li>
-                        </ul>
-                    </nav>
-                    <!-- PAGINATION END -->
-                </div>
+                <!-- POJEDYNCZE OGŁOSZENIE END -->
+                @endforeach
+            </div>
 
-                <!-- MAIN CONTENT END -->
-                <!-- PAGINATION START -->
+        </div>
+    </div>
+    <div class="fixed-bottom offset-md-2 p-4">
+        <nav aria-label="Strony">
+            <ul class="pagination justify-content-center">
+                <li class="page-item disabled">
+                    <a class="page-link" href="#" tabindex="-1">Poprzednia</a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item">
+                    <a class="page-link" href="#">Następna</a>
+                </li>
+            </ul>
+        </nav>
+        <!-- PAGINATION END -->
+    </div>
 
-            </main>
-   @endsection
+    <!-- MAIN CONTENT END -->
+    <!-- PAGINATION START -->
+
+</main>
+@endsection
