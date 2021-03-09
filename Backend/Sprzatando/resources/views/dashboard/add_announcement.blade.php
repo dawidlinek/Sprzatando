6498068294c22e30fed6ab709d9231dc0a770436
D3vmotiv <czechpiotr2701@gmail.com>
2021-03-09 12:34:29 +0100

    poprawki nad dodawniem zgloszen

--- Backend/Sprzatando/resources/views/dashboard/add_announcement.blade.php
+++ Backend/Sprzatando/resources/views/dashboard/add_announcement.blade.php
@@ -4,14 +4,31 @@
 <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
 
 <main class="col-md-9 col-sm-12 ms-sm-auto col-lg-10 px-md-4 bg-light">
-    <div class="d-flex justify-content-start flex-row flex-md-column align-items-center pt-3 pb-2 mb-3">
-        <div class="row w-100 match-height">
-            @include('dashboard.status')
-            <div class="card col-lg col-lg-12 d-flex flex-lg-row flex-md-column justify-content-around">
-                <div class="col-lg-5">
-                    <div class="card-body d-flex flex-column align-items-start justify-content-between">
-                        <h2 class="card-title text-primary mt-4 mb-4 ">Dodaj ogłoszenie</h2>
-                        <form class="w-100" method='POST' action='{{route('announcement.store')}}' enctype="multipart/form-data">
+
+    <!-- Spacer -->
+    <div class="pt-3 pb-2 mb-3"></div>
+
+    <div class="row w-100 match-height m-auto">
+        @include('dashboard.status')
+        <div class="card">
+
+            <!-- <Title  -->
+            <div class="row w-100">
+                <div class="col col-12 marginLeftDesktop">
+                    <h2 class="card-title text-primary mt-4 mb-4">Dodaj ogłoszenie</h2>
+                </div>
+            </div>
+            <!-- </Title  -->
+
+            <!-- <Main form> -->
+            <form class="w-100" method='POST' action='{{route('announcement.store')}}' enctype="multipart/form-data">
+
+                <div class="row w-100 d-flex flex-lg-row flex-md-column justify-content-around">
+
+                    <!-- Left column -->
+                    <div class="col-xl-5">
+                        <div class="d-flex flex-column align-items-start justify-content-between">
+
                             <label for="FormControlInput1 col-offset">Tytuł</label>
                             <input type="text" name='title' required class="form-control mb-4" />
                             <label for="FormControlInput1 col-offset">Lokalizacja</label>
@@ -22,91 +39,78 @@
                             <textarea maxlength="500" id="descriptionTA" name='description' required class="form-control mb-1" style="resize: none; height: 30vh;"></textarea>
 
                             <p>Pozostało <span id="signs">500</span> znaków</p>
+                        </div>
                     </div>
-                </div>
-                <div class="col-lg-5 d-flex align-items-center justify-content-center">
-                    <div class="card-body d-flex flex-column align-items-center justify-content-around h-100">
-                        {{-- <form class="w-100" > --}}
-                        @csrf
-                        <p>&nbsp;</p>
-                        <label for="FormControlInput1 col-offset mt-6">Czas ważności</label>
-                        <input type="date" name='expiring_at' class="form-control mt-1 mb-5" />
-                        <label for="FormControlInput1 col-offset">Kategoria:</label>
-                        {{-- <div class="d-flex flex-column">
-                                            <div class="d-flex">
-                                                <div class="w-33">
-                                                    <input type="checkbox" value="ogród"class="form-check-input mb-4" />
-                                                        <label for="ogród">Ogród</label>
-                                                </div>
-                                                <div class="w-33">
-                                                    <input type="checkbox" value="ogród"class="form-check-input mb-4" />
-                                                        <label for="ogród">Biuro</label>
-                                                </div>
-                                                <div class="w-33">
-                                                    <input type="checkbox" value="ogród"class="form-check-input mb-4" />
-                                                        <label for="ogród">Dachy</label>
-                                                </div>
-                                            </div>
-                                            <div class="d-flex">
-                                                <div class=" w-33">
-                                                    <input type="checkbox" value="ogród"class="form-check-input mb-4" />
-                                                        <label for="ogród">Mieszkanie</label>
-                                                </div>
-                                                <div class=" w-33">
-                                                    <input type="checkbox" value="ogród"class="form-check-input mb-4" />
-                                                        <label for="ogród">Mycie okien</label>
-                                                </div>
-                                            </div>
-                                        </div> --}}
-                        <select name='category_id' class="form-select mb-5 mt-1">
-                            @foreach ($categories as $category)
-                            <option value="{{$category->id}}">{{$category->name}}</option>
 
-                            @endforeach
 
-                        </select>
-                        <label for="FormControlInput1 col-offset" class="mb-3">Dodaj zdjęcia:</label> <br />
+                    <!-- Right column -->
+                    <div class="col-xl-5">
+                        <div class="d-flex h-100 flex-column align-items-start justify-content-between">
 
-                        <div class="custom-file d-flex justify-content-between align-items-start w-100 mb-5" style="height: 15vh;">
-                            <div class="w-25">
-                                <input class="form-control mb-3" type="file" id="formFileDisabled1"/>
-                                <label class="form-check-label position-relative ramka-image" for="formFileDisabled1">
-                                <div class="add-image" >
-                                <img src="../assets/img/rec.png" height="150px" width="150px" id="first-image" class="img-fluid add-image zIndex2" draggable="false"/>
-                                    <div class="plus-add" id="sectionAddFirstImage">+</div>
+                            <label for="FormControlInput1 col-offset mt-6">Czas ważności</label>
+                            <input type="date" name='expiring_at' class="form-control mb-4" />
+                            <label for="FormControlInput1 col-offset">Kategoria:</label>
+
+                            <!-- <Kategorie> -->
+                            <select name='category_id' class="form-select mb-4">
+                                @foreach ($categories as $category)
+                                <option value="{{$category->id}}">{{$category->name}}</option>
+
+                                @endforeach
+                            </select>
+                            <!-- </Kategorie> -->
+
+
+
+                            <!-- <Image select> -->
+                            <label for="FormControlInput1 col-offset" class="mb-3">Dodaj zdjęcia:</label> <br />
+                            <div class="custom-file d-flex justify-content-between align-items-start w-100 mb-2 mb-xl-5" style="height: 15vh;">
+                                <div class="w-33">
+                                    <input class="form-control mb-3" type="file" id="formFileDisabled1" />
+                                    <label class="form-check-label position-relative ramka-image" for="formFileDisabled1">
+                                        <div class="add-image">
+                                            <img src="../assets/img/rec.png" height="150px" width="150px" id="first-image" class="img-fluid add-image zIndex2" draggable="false" />
+                                            <div class="plus-add" id="sectionAddFirstImage">+</div>
+                                        </div>
+                                        <div class="delete-image w-100" id="first-delete-image">Usuń zdjęcie</div>
+                                    </label>
                                 </div>
-                                <div class="delete-image w-100" id="first-delete-image">Usuń zdjęcie</div>
-                            </label>
-                            </div>
-                            <div class="w-25  d-flex flex-column">
-                                <input class="form-control mb-3" type="file" id="formFileDisabled2" />
-                                <label class="form-check-label position-relative ramka-image" for="formFileDisabled2">
-                                <div class="add-image">
-                                <img src="../assets/img/rec.png" height="150px" width="150px" id="second-image" class="img-fluid add-image zIndex2" draggable="false"/>
-                                    <div class="plus-add" id="sectionAddSecondImage">+</div>
+
+                                <div class="w-33 d-flex flex-column">
+                                    <input class="form-control mb-3" type="file" id="formFileDisabled2" />
+                                    <label class="form-check-label position-relative ramka-image" for="formFileDisabled2">
+                                        <div class="add-image">
+                                            <img src="../assets/img/rec.png" height="150px" width="150px" id="second-image" class="img-fluid add-image zIndex2" draggable="false" />
+                                            <div class="plus-add" id="sectionAddSecondImage">+</div>
+                                        </div>
+                                        <div class="delete-image w-100" id="second-delete-image">Usuń zdjęcie</div>
+                                    </label>
                                 </div>
-                                <div class="delete-image w-100" id="second-delete-image">Usuń zdjęcie</div>
-                            </label>
-                            </div>
-                            <div class="w-25">
-                                <input class="form-control mb-3" type="file" id="formFileDisabled3" />
-                                <label class="form-check-label position-relative ramka-image" for="formFileDisabled3">
-                                <div class="add-image" >
-                                <img src="../assets/img/rec.png" height="150px" width="150px" id="third-image" class="img-fluid add-image zIndex2" draggable="false"/>
-                                    <div class="plus-add" id="sectionAddThirdImage">+</div>
+
+                                <div class="w-33">
+                                    <input class="form-control mb-3" type="file" id="formFileDisabled3" />
+                                    <label class="form-check-label position-relative ramka-image" for="formFileDisabled3">
+                                        <div class="add-image">
+                                            <img src="../assets/img/rec.png" height="150px" width="150px" id="third-image" class="img-fluid add-image zIndex2" draggable="false" />
+                                            <div class="plus-add" id="sectionAddThirdImage">+</div>
+                                        </div>
+                                        <div class="delete-image w-100" id="third-delete-image">Usuń zdjęcie</div>
+                                    </label>
                                 </div>
-                                <div class="delete-image w-100" id="third-delete-image">Usuń zdjęcie</div>
-                            </label>
                             </div>
-                        </div><br />
-                        <button type='submit' class="btn btn-primary w-100 mt-5 mb-1 text-white">Dodaj</a>
-                            </form>
+                            <!-- </Image select> -->
+
+                            <div class="w-100" style="margin-bottom: 2.8rem;">
+                                <button type='submit' class="btn btn-primary w-100 mt-3 text-white">Dodaj</a>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-
+            </form>
+            <!-- </Main form> -->
 
         </div>
+    </div>
+    </div>
     </div>
     </div>
     </div>
