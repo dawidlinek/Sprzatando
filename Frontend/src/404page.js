74a81da21adb74e6c9ddfc840ce13d7288db4e95
cotlowzky <cotlowzky@gmail.com>
2021-03-09 11:57:31 +0100

    404 page last

---
+++ Frontend/src/404page.js
@@ -0,0 +1,17 @@
+const firstNumber = document.querySelector('#firstNum');
+const secondNumber = document.querySelector('#secondNum');
+const thirdNumber = document.querySelector('#thirdNum');
+let i = 0;
+function randomNumber(){
+    if(i > 20){
+        firstNumber.innerHTML = "4";
+        secondNumber.innerHTML ="0"
+        thirdNumber.innerHTML ="4";
+    }else{
+    firstNumber.innerHTML = Math.floor(Math.random() * 9);
+    secondNumber.innerHTML = Math.floor(Math.random() * 9);
+    thirdNumber.innerHTML = Math.floor(Math.random() * 9);
+    i++
+    }
+}
+setInterval(randomNumber,100);
