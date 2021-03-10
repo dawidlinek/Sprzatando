54f58653467717ff495bc923b0410bceb267a9ec
dawidlinek123 <56173086+dawidlinek123@users.noreply.github.com>
2021-03-10 21:31:12 +0100

    500 error page fix

--- Backend/Sprzatando/resources/views/errors/500.blade.php
+++ Backend/Sprzatando/resources/views/errors/500.blade.php
@@ -5,7 +5,7 @@
      <main class="col-md-12 ms-sm-auto col-lg-12 px-md-4 bg-light bg-light h-100 w-100 d-flex align-items-center justify-content-center text-center" >
         <div class="card w-75 h-75 d-flex flex-column align-items-center justify-content-center">
         <h1 style="font-size: 80px;"><span id="firstNum">5</span><span id="secondNum">0</span><span id="thirdNum">0</span></h1>
-        <h2 class="mt-3">Strona wygasła</h2>
+        <h2 class="mt-3">Wystąpił błąd</h2>
         <h4 class="mt-3">Niestety na stronie, którą chcesz odwiedzić wystąpił błąd. <br />
             Spróbuj ponownie lub skontaktuj się z administratorem.
         </h4>
