77153abbe59b477d8adea55bd9238712baea004f
dawidlinek123 <56173086+dawidlinek123@users.noreply.github.com>
2021-03-10 20:24:38 +0100

    tłumaczenia

--- Backend/Sprzatando/resources/lang/pl/validation.php
+++ Backend/Sprzatando/resources/lang/pl/validation.php
@@ -2,7 +2,7 @@
 'unique' => 'Nazwa użytkownika i email musi być unikatowa',
 'confirmed'=>"Hasła muszą się zgadzać",
 'min' => [
-    'numeric' => 'The :attribute must be at least :min.',
+    'numeric' => 'Pole :attribute musi mieć co najmniej :min znaków.',
     'file' => 'The :attribute must be at least :min kilobytes.',
     'string' => 'Musi mieć conajmniej 8 znków.',
     'array' => 'The :attribute must have at least :min items.',
@@ -10,8 +10,9 @@
 'max'=>[
     "string"=>"Wartość pola :attribute jest za długa",
 ],
+'numeric'=>"Pole musi być liczbą",
 'exists'=>"Podana wartość nie instnieje",
 'required' => 'Pole :attribute jest wymagane.',
-'attributes' => ['name'=>"nazwa",'old_password'=>"Stare hasło",'confirmed'=>"Powtórz hasło",'password'=>"Hasło",'title'=>"Tytuł",'description'=>"Opis",'categories'=>"Kategorii"],
+'attributes' => ['name'=>"nazwa",'old_password'=>"Stare hasło",'confirmed'=>"Powtórz hasło",'password'=>"Hasło",'title'=>"Tytuł",'description'=>"Opis",'price'=>"Cena",'localization'=>"Lokalizacja",'categories'=>"Kategorii"],
 ];
 ?>
