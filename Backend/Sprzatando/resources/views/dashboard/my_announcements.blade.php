dbe04e2a39d388a12037691288ed7e3c573d69df
D3vmotiv <czechpiotr2701@gmail.com>
2021-03-10 09:33:21 +0100

    moje ogloszenia

--- Backend/Sprzatando/resources/views/dashboard/my_announcements.blade.php
+++ Backend/Sprzatando/resources/views/dashboard/my_announcements.blade.php
@@ -15,81 +15,52 @@
                 @endif
                 <!-- POJEDYNCZE OGŁOSZENIE -->
                 @foreach ($announcements as $announcement)
-
-
-
-
-
-
-                {{-- <div class="card d-flex w-100 mt-3">
-                                    <div class="row w-100 mx-auto"> --}}
-                {{-- <div class="col-md-2 rounded" style="background-image: url({{$announcement->main_image}}); background-position: center center; background-size: cover; min-height: 200px;">
-                &nbsp;
-            </div>
-            <div class="col-md-8 col-sm-8">
-                <div class="card-body">
-                    <div class="row col-12">
-                        <div class="col-lg-6">
-                            <h5 class="card-title text-primary text-nowrap">{{$announcement->title}}</h5>
+                <div class="card d-flex w-100 mt-3">
+                    <div class="row w-100 mx-auto">
+                        <div class="col-md-auto rounded" style="background-image: url({{$announcement->main_image}}); background-position: center center; background-size: cover; min-height: 200px; min-width: 200px;">
+                            &nbsp;
                         </div>
-                        <div class="col-lg-4">
-                            <p class="card-text nowrap d-sm-flex"><small class="text-muted d-md-none justify-content-start">{{$announcement->localization}}</small><small class="text-muted d-flex justify-content-around">{{$announcement->price}} zł</small>
-                            </p>
-
-                        </div>
-                    </div>
-                    <div class="w-100">
-                        <p class="card-text">
-                            <b-badge><small class="text-danger">b-badge {{$announcement->status}}</small></b-badge>
-                        </p>
-                    </div> --}}
-
-                    <div class="card d-flex w-100 mt-3">
-                        <div class="row w-100 mx-auto">
-                            <div class="col-md-2 rounded" style="background-image: url({{$announcement->main_image}}); background-position: center center; background-size: cover; min-height: 200px;">
-                                &nbsp;
-                            </div>
-                            <div class="col-md-8 col-sm-8">
-                                <div class="card-body">
-                                    <div class="row col-12">
-                                        <div class="col-lg-6">
-                                            <h5 class="card-title text-primary text-nowrap">{{$announcement->title}}</h5>
-                                        </div>
-                                        <div class="col-lg-4">
-                                            <p class="card-text nowrap d-sm-flex"><small class="text-muted d-md-none justify-content-start">{{$announcement->localization}}</small><small class="text-muted d-flex justify-content-around">{{$announcement->price}} zł</small>
-                                            </p>
-
-                                        </div>
+                        <div class="col-md-6 col-sm-8">
+                            <div class="card-body">
+                                <div class="row col-12">
+                                    <div class="col-lg-6">
+                                        <h5 class="card-title text-primary text-nowrap">{{$announcement->title}}</h5>
                                     </div>
-                                    <div class="w-100">
-                                        <p class="card-text">
-                                            <b-badge><small class="text-danger">b-badge {{$announcement->status}}</small></b-badge>
+                                    <div class="col-lg-4">
+                                        <p class="card-text nowrap d-sm-flex"><small class="text-muted d-md-none justify-content-start">{{$announcement->localization}}</small><small class="text-muted d-flex justify-content-around">{{$announcement->price}} zł</small>
                                         </p>
+
                                     </div>
-
-                                    <p class="card-text">{{$announcement->description}} </p>
                                 </div>
-                            </div>
-                            <div class="col-md-2 p-4">
-                                <div class="b-row d-md-block d-none">
-                                    <p><small class="text-muted overflow-wrap">{{$announcement->localization}}</small></p>
-                                </div>
-                                <div class="b-row d-flex align-text-center p-3">
-                                    <a class="btn btn-primary w-100 text-nowrap text-white" href="{{ route('announcement.edit', $announcement->id) }}">Edytuj</a>
+                                <div class="w-100">
+                                    <p class="card-text">
+                                        <b-badge><small class="text-danger">b-badge {{$announcement->status}}</small></b-badge>
+                                    </p>
                                 </div>
 
+                                <p class="card-text">{{$announcement->description}} </p>
                             </div>
                         </div>
-                    </div>
-                    <!-- POJEDYNCZE OGŁOSZENIE END -->
+                        <div class="col-md-3 p-4">
+                            <div class="b-row d-md-block d-none">
+                                <p><small class="text-muted overflow-wrap">{{$announcement->localization}}</small></p>
+                            </div>
+                            <div class="b-row d-flex align-text-center p-3">
+                                <a class="btn btn-primary w-100 text-nowrap text-white" href="{{ route('announcement.edit', $announcement->id) }}">Edytuj</a>
+                            </div>
 
-                    @endforeach
+                        </div>
+                    </div>
                 </div>
+                <!-- POJEDYNCZE OGŁOSZENIE END -->
+
+                @endforeach
             </div>
         </div>
+    </div>
 
 
-        {{-- <div class="fixed-bottom offset-md-2 p-4">
+    {{-- <div class="fixed-bottom offset-md-2 p-4">
                                         <nav aria-label="Strony">
                                             <ul class="pagination justify-content-center">
                                                 <li class="page-item disabled">
@@ -106,8 +77,8 @@
                                         <!-- PAGINATION END -->
                                     </div> --}}
 
-        <!-- MAIN CONTENT END -->
-        <!-- PAGINATION START -->
+    <!-- MAIN CONTENT END -->
+    <!-- PAGINATION START -->
 
 </main>
 @endsection
