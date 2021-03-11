8e362d96125dbb8ea7a077855c184c27b17b633a
dawidlinek123 <56173086+dawidlinek123@users.noreply.github.com>
2021-03-11 10:37:46 +0100

    display block

--- Backend/Sprzatando/resources/views/welcome.blade.php
+++ Backend/Sprzatando/resources/views/welcome.blade.php
@@ -23,7 +23,7 @@
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
             @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                <div class="hidden fixed top-0  right-0 px-6 py-4">
                     @auth
                         <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Panel</a>
                     @else
