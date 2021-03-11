a20b227b5ee541fee263d84673383ace398f6f3a
dawidlinek123 <56173086+dawidlinek123@users.noreply.github.com>
2021-03-11 12:36:39 +0100

    403

--- Backend/Sprzatando/resources/views/errors/403.blade.php
+++ Backend/Sprzatando/resources/views/errors/403.blade.php
@@ -4,7 +4,7 @@
 <body style="height: 100vh; width: 100vw;">
      <main class="col-md-12 ms-sm-auto col-lg-12 px-md-4 bg-light bg-light h-100 w-100 d-flex align-items-center justify-content-center text-center" >
         <div class="card w-75 h-75 d-flex flex-column align-items-center justify-content-center">
-        <h1 style="font-size: 80px;"><span id="firstNum">4</span><span id="secondNum">0</span><span id="thirdNum">1</span></h1>
+        <h1 style="font-size: 80px;"><span id="firstNum">4</span><span id="secondNum">0</span><span id="thirdNum">3</span></h1>
         <h2 class="mt-3">Nieautoryzowany dostęp</h2>
         <h4 class="mt-3">Nie masz dostępu do tego zasobu. <br />
             Jeśli ten błąd wystąpił przy weryfikacji email, pamiętaj że link aktywacyjny musisz otworzyć w zalogowanej sesji. 
