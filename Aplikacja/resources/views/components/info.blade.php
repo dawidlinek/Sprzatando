d6566800463c478cea0459633a01b5d366da59ce
dawidlinek123 <56173086+dawidlinek123@users.noreply.github.com>
2021-04-04 21:51:37 +0200

    info component

--- Aplikacja/resources/views/components/info.blade.php
+++ Aplikacja/resources/views/components/info.blade.php
@@ -6,9 +6,11 @@
 </div>
 @endif
 @if ($errors->any())
-<div class="alert alert-danger mb-2 mt-4 col-10 mx-auto border-0 p-2" role="alert">
+<div class="row w-100 match-height d-flex justify-content-center" style="padding-right: 1rem; padding-left: 1rem;">
+<div class="alert alert-danger mb-4 mt-4 col-12 border-0" role="alert">
     @foreach ($errors->all() as $error)
     {{ $error }}
     @endforeach
+</div>
 </div>
 @endif
