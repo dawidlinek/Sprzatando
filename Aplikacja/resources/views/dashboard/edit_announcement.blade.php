1815bdaea09f14ae517911d60a8b5d0b7103dbf0
dawidlinek123 <56173086+dawidlinek123@users.noreply.github.com>
2021-04-20 12:16:37 +0200

    fix

--- Aplikacja/resources/views/dashboard/edit_announcement.blade.php
+++ Aplikacja/resources/views/dashboard/edit_announcement.blade.php
@@ -226,7 +226,7 @@
                         </div>
                         <div class="mt-1 mb-3">
                             <h2 class="text-primary">Ostatnie zlecenie</h2>
-                            <p id="detailDescription">{{$user->engaged()->where('status','selected')->latest()->first()->details->}}</p>
+                            <p id="detailDescription">{{$user->engaged()->where('status','selected')->latest()->first()->details->rating_description}}</p>
                         </div> 
                         <div class="mt-1 mb-5">
                             <h2 class="text-primary">Statystyki użytkownika</h2>
