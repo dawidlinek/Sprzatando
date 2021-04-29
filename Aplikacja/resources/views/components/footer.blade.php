e3ed18718d6be8172f33ac60cd35a6b2d8557dfa
D3vmotiv <czechpiotr2701@gmail.com>
2021-04-29 11:22:04 +0200

    Poprawki

--- Aplikacja/resources/views/components/footer.blade.php
+++ Aplikacja/resources/views/components/footer.blade.php
@@ -1,46 +1,48 @@
 <footer>
     <!-- FOOTER START -->
     <div class="row">
-        <div class="p-3 text-light" style="background-color: #15192F; z-index: 1000;">
-            <div class="col-12 col-xl-10 offset-0 offset-xl-1">
-                <div class="row m-3 m-md-5 mb-0">
+        <div class="col-12">
+            <div class="p-3 text-light" style="background-color: #15192F; z-index: 1000;">
+                <div class="col-12 col-xl-10 offset-0 offset-xl-1">
+                    <div class="row m-3 m-md-5 mb-0">
 
-                    <div class="col-lg-8 col-sm-12">
+                        <div class="col-lg-8 col-sm-12">
 
-                        <div class="row mx-auto d-flex justify-content-center">
-                            <a href="/" class="text-white text-decoration-none d-flex align-items-center justify-content-center justify-content-lg-start m-3 m-md-0 p-0">
-                                <img  alt='logo' style="margin-right: 0.5rem;" width="95px" height="95px" src="/img/logo_invert.png">
-                                <h1 class="align-bottom footer-logo-text">SPRZATNIJ.ME</h1>
-                            </a>
+                            <div class="row mx-auto d-flex justify-content-center">
+                                <a href="/" class="text-white text-decoration-none d-flex align-items-center justify-content-center justify-content-lg-start m-3 m-md-0 p-0">
+                                    <img  alt='logo' style="margin-right: 0.5rem;" width="95px" height="95px" src="/img/logo_invert.png">
+                                    <h1 class="align-bottom footer-logo-text">SPRZATNIJ.ME</h1>
+                                </a>
+                            </div>
+
                         </div>
+                        <div class="col-lg-4 col-sm-12 text-center text-lg-end">
+                            <a href="/" class="text-white text-decoration-none">
+                                <p>Strona główna</p>
+                            </a>
+                            <a href="/search" class="text-white text-decoration-none">
+                                <p>Przeglądaj oferty</p>
+                            </a>
+                            <a href="/ranking" class="text-white text-decoration-none">
+                                <p>Ranking użytkowników</p>
+                            </a>
 
+                        </div>
                     </div>
-                    <div class="col-lg-4 col-sm-12 text-center text-lg-end">
-                        <a href="/" class="text-white text-decoration-none">
-                            <p>Strona główna</p>
-                        </a>
-                        <a href="/search" class="text-white text-decoration-none">
-                            <p>Przeglądaj oferty</p>
-                        </a>
-                        <a href="/ranking" class="text-white text-decoration-none">
-                            <p>Ranking użytkowników</p>
-                        </a>
-
-                    </div>
-                </div>
-                <div class="row m-5 mt-0 mb-3">
-                    <div class="col-lg-8 col-sm-12 d-flex flex-column justify-content-between text-center text-lg-start mb-3 mb-lg-0">
-                        <p>Znajdziesz nas także na:</p>
-                        <div class="row justify-content-center justify-content-lg-start">
-                            <div class="col-lg-6 col-sm-8 d-flex justify-content-center justify-content-lg-start">
-                                <a href="https://github.com/dawidlinek123/Sprzatando" style="margin-right: 0.5rem;" rel="noopener" target="_blank"  aria-label="Github"><img width="48px" height="48px" alt='github' src="/img/GitHub-Mark-Light-64px.png"></a>
-                                <a href="https://www.facebook.com/ReniaKisza" style="margin-left: 0.5rem;" rel="noopener" target="_blank"  aria-label="Facebook"><img   alt='facebook' width="48px" height="48px" src="/img/facebook.png"></a>
+                    <div class="row m-5 mt-0 mb-3">
+                        <div class="col-lg-8 col-sm-12 d-flex flex-column justify-content-between text-center text-lg-start mb-3 mb-lg-0">
+                            <p>Znajdziesz nas także na:</p>
+                            <div class="row justify-content-center justify-content-lg-start">
+                                <div class="col-lg-6 col-sm-8 d-flex justify-content-center justify-content-lg-start">
+                                    <a href="https://github.com/dawidlinek123/Sprzatando" style="margin-right: 0.5rem;" rel="noopener" target="_blank"  aria-label="Github"><img width="48px" height="48px" alt='github' src="/img/GitHub-Mark-Light-64px.png"></a>
+                                    <a href="https://www.facebook.com/ReniaKisza" style="margin-left: 0.5rem;" rel="noopener" target="_blank"  aria-label="Facebook"><img   alt='facebook' width="48px" height="48px" src="/img/facebook.png"></a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-lg-4 col-sm-12 text-center text-lg-end d-flex flex-column justify-content-end" style="border-top: 2px solid #fff;">
-                        <p style="margin-top: 16px;"><a href="https://youtu.be/dQw4w9WgXcQ" class="text-white text-decoration-none" rel="noopener" target="_blank">Polityka plików cookies</a></p>
-                        <p style="margin-bottom: 0;"><a href="https://youtu.be/dQw4w9WgXcQ" class="text-white text-decoration-none" rel="noopener" target="_blank">Regulamin i polityka prywatności</a></p>
+                        <div class="col-lg-4 col-sm-12 text-center text-lg-end d-flex flex-column justify-content-end" style="border-top: 2px solid #fff;">
+                            <p style="margin-top: 16px;"><a href="https://youtu.be/dQw4w9WgXcQ" class="text-white text-decoration-none" rel="noopener" target="_blank">Polityka plików cookies</a></p>
+                            <p style="margin-bottom: 0;"><a href="https://youtu.be/dQw4w9WgXcQ" class="text-white text-decoration-none" rel="noopener" target="_blank">Regulamin i polityka prywatności</a></p>
+                        </div>
                     </div>
                 </div>
             </div>
